b99a8a4d339e8566b5ce50f192f1c79a8218ee85
Kim YoungJin <smilup2244@gmail.com>
2024-03-13 11:31:49 +0900

    [#10741] Added applicationId

--- web/src/main/java/com/navercorp/pinpoint/web/applicationmap/service/ResponseTimeHistogramServiceImpl.java
+++ web/src/main/java/com/navercorp/pinpoint/web/applicationmap/service/ResponseTimeHistogramServiceImpl.java
@@ -106,7 +106,7 @@ public class ResponseTimeHistogramServiceImpl implements ResponseTimeHistogramSe
     public NodeHistogramSummary selectNodeHistogramData(ResponseTimeHistogramServiceOption option) {
         Node node = new Node(option.getApplication());
         Application application = option.getApplication();
-        ServiceType applicationServiceType = application.getServiceType();
+        ServiceType applicationServiceType = application.serviceType();
 
         List<Application> sourceApplications = option.getFromApplications();
         List<Application> destinationApplications = option.getToApplications();
@@ -188,7 +188,7 @@ public class ResponseTimeHistogramServiceImpl implements ResponseTimeHistogramSe
 
 
         LinkDataDuplexMap linkDataDuplexMap;
-        ServiceType fromApplicationServiceType = fromApplication.getServiceType();
+        ServiceType fromApplicationServiceType = fromApplication.serviceType();
         LinkDirection linkDirection = LinkDirection.OUT_LINK;
         if (fromApplicationServiceType.isUser()) {
             //scan using toApplication to distinguish same applicationName with different serviceType
